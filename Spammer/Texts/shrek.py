ccdbdc249c5b01e196de52348f8c9a94e801b08a
EpicGiant27 <82335462+EpicGiant27@users.noreply.github.com>
2021-04-11 19:47:40 +0100

    Add files via upload

---
+++ Spammer/Texts/shrek.py
@@ -0,0 +1,16 @@
+import time
+import pyautogui
+import sys
+
+def SendScript(filename: str):
+	time.sleep(5)
+	with open(filename) as f:
+		lines = f.readlines()
+	for line in lines:
+		pyautogui.typewrite(line.strip())
+		pyautogui.press('enter')
+
+if __name__ == "__main__":
+	if(len(sys.argv) < 2):
+		SendScript("script.txt")
+	SendScript(sys.argv[1])
